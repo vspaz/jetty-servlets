38de5d99066517237320a626fd415ed652fbd113
vspaz <vladimir.edelman@gmail.com>
2021-02-28 22:12:37 +0100

    add logging

--- basicServer/src/main/java/main/Main.java
+++ basicServer/src/main/java/main/Main.java
@@ -3,20 +3,24 @@ package main;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.Logger;
 import servlets.*;
 import servlets.servers.*;
 
 
 public class Main {
     public static void main(String[] args) throws Exception {
+        Logger logger = Log.getRootLogger();
+        logger.info("starting server...");
         ServletContextHandler ctx = new ServletContextHandler(ServletContextHandler.SESSIONS);
 
         Index indexServlet = new Index();
-        ctx.addServlet(new ServletHolder(indexServlet), "/");
-        ctx.addServlet(new ServletHolder(indexServlet), "/index");
 
         ctx.addServlet(new ServletHolder(new Contact()), "/contacts");
-        ctx.addServlet(new ServletHolder(new VM()), "/servers");
+        ctx.addServlet(new ServletHolder(indexServlet),  "/index");
+        ctx.addServlet(new ServletHolder(new VM()),      "/servers");
+        ctx.addServlet(new ServletHolder(indexServlet),  "/");
 
         org.eclipse.jetty.server.Server server = new org.eclipse.jetty.server.Server(8080);
         server.setHandler(ctx);
--- basicServer/src/main/java/servlets/Index.java
+++ basicServer/src/main/java/servlets/Index.java
@@ -5,9 +5,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.log.Logger;
+
+
 public class Index extends HttpServlet {
+    public static final Logger logger = Log.getLogger(Index.class.getName());
+
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        logger.info(
+                String.format(
+                        "%s.%s called",
+                        Index.class.getName(),
+                        Thread.currentThread().getStackTrace()[1].getMethodName()
+                )
+        );
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         response.getWriter().println("index");
