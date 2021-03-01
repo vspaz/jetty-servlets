6a54546cae1ff29c487851e3e282a855fe8a99bf
vspaz <vladimir.edelman@gmail.com>
2021-03-01 12:07:01 +0100

    remove unused exception

--- basicServer/src/main/java/servlets/Login.java
+++ basicServer/src/main/java/servlets/Login.java
@@ -1,11 +1,11 @@
 package servlets;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+
 
 public class Login extends HttpServlet {
     private static final String DEFAULT_USER = "guest";
@@ -36,6 +36,7 @@ public class Login extends HttpServlet {
                 .append("  </body>\r\n")
                 .append("</html>");
     }
+
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         this.doGet(request, response);
