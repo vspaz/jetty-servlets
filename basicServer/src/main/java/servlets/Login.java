e892d51943dac149ddc6eb79cc2e4dca0e9d1076
vspaz <vladimir.edelman@gmail.com>
2021-03-01 11:55:54 +0100

    add redirect path

--- basicServer/src/main/java/servlets/Login.java
+++ basicServer/src/main/java/servlets/Login.java
@@ -26,7 +26,7 @@ public class Login extends HttpServlet {
                 .append("  <head>\r\n")
                 .append("  <body>\r\n")
                 .append("    Hi, \r\n").append(user).append("<br></br>\r\n")
-                .append("    <form action=\"greeting\" method=\"POST\">\r\n")
+                .append("    <form action=\"login\" method=\"POST\">\r\n")
                 .append("      Please enter your name: <br />\r\n")
                 .append("      <input type=\"text\" name=\"user\" ><br />\r\n")
                 .append("      Please enter your password: <br />\r\n")
